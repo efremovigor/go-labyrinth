eb2b2581e317e2403ff4f3f84b2104e4377f7971
igore <igor198811@gmail.com>
2018-05-06 01:24:19 +0300

    generate random labyrinth

--- main.go
+++ main.go
@@ -5,16 +5,29 @@ import (
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
-	"image/color"
 	"strconv"
 	"math/rand"
-	"fmt"
 	"time"
+	"image/color"
+	"fmt"
 )
 
+var src = rand.NewSource(time.Now().UnixNano())
+var r = rand.New(src)
+
 var factor = 10
+var lengthX = 99
+var lengthY = 71
 var collection = map[string]Square{}
-var beginIndex string
+var beginIndex = "1|1"
+var currentIndex string
+var crossways []string
+var colorMap = map[int]color.RGBA{
+	0: colornames.Black,
+	1: colornames.White,
+	2: colornames.Aqua,
+	11: colornames.Red,
+}
 
 type Square struct {
 	state int
@@ -22,13 +35,19 @@ type Square struct {
 	rect  pixel.Rect
 	x     int
 	y     int
+	pass  bool
+}
+
+func (s *Square) getColor() color.RGBA {
+	return colorMap[s.state]
 }
 
 func getIndex(x int, y int) string {
 	return strconv.Itoa(x) + "|" + strconv.Itoa(y)
 }
 
-func generatePattern(lengthX int, lengthY int) {
+func generate(lengthX int, lengthY int) {
+	//Create blank
 	collection = make(map[string]Square)
 	for y := 0; y < lengthY; y++ {
 		for x := 0; x < lengthX; x++ {
@@ -37,31 +56,92 @@ func generatePattern(lengthX int, lengthY int) {
 			if x+1 == lengthX || x == 0 || y+1 == lengthY || y == 0 || x%2 != 1 || y%2 != 1 {
 				square.state = 0
 			} else {
-				if beginIndex != "" && rand.Intn(2) == 0 {
+				if beginIndex == "" && randBool((lengthX+lengthY)/2) {
 					beginIndex = getIndex(x, y)
+				} else {
+					square.state = 1
 				}
-				square.state = 1
 			}
 			collection[getIndex(x, y)] = square
 		}
 	}
+	//Create paths
+	currentIndex = beginIndex
+	updateWay(currentIndex)
 
+	for len(crossways) > 0 {
+		crossway := crossways[0]
+		crossways = crossways[1:]
+		updateWay(crossway)
+
+	}
 	return
 }
 
-func getSiblings() (list []string) {
-	rect := collection[beginIndex]
-	if _, ok := collection[getIndex(rect.x, rect.y-2)]; ok {
+func setPass(index string) {
+	element := collection[index]
+	element.state = 1
+	element.pass = true
+	collection[index] = element
+}
+
+func updateWay(indexWay string){
+	siblings := getSiblings(indexWay)
+	if len(siblings) > 1 {
+		crossways = append(crossways,currentIndex)
+	}
+	for len(siblings) > 0 {
+		if len(siblings) > 1 {
+			crossways = append(crossways,indexWay)
+		}
+		if len(siblings) > 0 {
+			nextIndex := siblings[r.Intn(len(siblings))]
+			for _, index := range getMediator(collection[indexWay], collection[nextIndex]) {
+				setPass(index)
+				fmt.Println(index)
+				indexWay = index
+			}
+		}
+		siblings = getSiblings(indexWay)
+	}
+}
+
+func getSiblings(index string) (list []string) {
+	rect := collection[index]
+	if current, ok := collection[getIndex(rect.x, rect.y-2)]; ok && current.pass == false {
 		list = append(list, getIndex(rect.x, rect.y-2))
 	}
-	if _, ok := collection[getIndex(rect.x, rect.y+2)]; ok {
+	if current, ok := collection[getIndex(rect.x, rect.y+2)]; ok && current.pass == false {
 		list = append(list, getIndex(rect.x, rect.y+2))
 	}
-	if _, ok := collection[getIndex(rect.x-2, rect.y)]; ok {
+	if current, ok := collection[getIndex(rect.x-2, rect.y)]; ok && current.pass == false {
 		list = append(list, getIndex(rect.x-2, rect.y))
 	}
-	if _, ok := collection[getIndex(rect.x+2, rect.y)]; ok {
+	if current, ok := collection[getIndex(rect.x+2, rect.y)]; ok && current.pass == false {
 		list = append(list, getIndex(rect.x+2, rect.y))
+	}
+	return
+}
+
+func getMediator(square1 Square, square2 Square) (list []string) {
+	for square1.x > square2.x {
+		square1.x--
+		list = append(list,getIndex(square1.x, square1.y))
+	}
+
+	for square1.x < square2.x {
+		square1.x++
+		list = append(list,getIndex(square1.x, square1.y))
+	}
+
+	for square1.y > square2.y {
+		square1.y--
+		list = append(list,getIndex(square1.x, square1.y))
+	}
+
+	for square1.y < square2.y {
+		square1.y++
+		list = append(list,getIndex(square1.x, square1.y))
 	}
 	return
 }
@@ -78,9 +158,9 @@ func run() {
 	}
 	imd := imdraw.New(win)
 	for _, square := range collection {
-		imd.Color = colornames.Black
+		imd.Color = square.getColor()
 		imd.Push(square.rect.Min, square.rect.Max)
-		imd.Rectangle(float64(square.state))
+		imd.Rectangle(0)
 	}
 
 	for !win.Closed() {
@@ -91,19 +171,10 @@ func run() {
 }
 
 func main() {
-	generatePattern(101, 77)
-	checkPointBegin()
+	generate(lengthX, lengthY)
 	pixelgl.Run(run)
 }
 
-func randBool() bool {
-	var src = rand.NewSource(time.Now().UnixNano())
-	var r = rand.New(src)
-	return r.Intn(2) != 0
-
-}
-
-func checkPointBegin() {
-	r := rand.New(rand.NewSource(99))
-	fmt.Println(r.Int())
+func randBool(limit int) bool {
+	return r.Intn(limit) == 0
 }
